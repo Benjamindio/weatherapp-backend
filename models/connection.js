1afc835e6a8dcefaa66e66e3488ea5865aac874a
Benjamin Diomat <benjamin.diomat@gmail.com>
2023-06-23 20:33:52 +0200

    commit

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.CONNECTION_STRING;
+const connectionString = 'mongodb+srv://admin:JkdpN3UOm33Md4A0@myfirstdatabase.s9xahi1.mongodb.net/weatherapp';
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
