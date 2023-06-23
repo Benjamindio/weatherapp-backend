6bd72865f4737918a4ae7b56b9e4c03dc5862897
Benjamin Diomat <benjamin.diomat@gmail.com>
2023-06-23 20:24:48 +0200

    commit

--- models/connection.js
+++ models/connection.js
@@ -6,4 +6,3 @@ mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
   .catch(error => console.error(error));
   
-module.exports = mongoose;
